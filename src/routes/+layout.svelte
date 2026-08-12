9c32283589d2e641e3f8ecb9e9e0242d57b1a305
vercelsetup-pat <vercelsetup@gmail.com>
2026-08-12 07:48:05 +0300

    update image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,8 +12,8 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="Omar & Fatima Wedding" />
 	<meta property="og:description" content="23 August 2026 • Jupiter Bnachei, Lebanon"/>
-	<meta property="og:image" content="https://fatimaomar-wedding.vercel.app//envAr.png"/>
-	<meta property="og:image:secure_url" content="https://fatimaomar-wedding.vercel.app//envAr.png"/>
+	<meta property="og:image" content="https://fatimaomar-wedding.vercel.app//card.png"/>
+	<meta property="og:image:secure_url" content="https://fatimaomar-wedding.vercel.app//card.png"/>
 
 	<meta property="og:image:type" content="image/png" />
 	<meta property="og:image:width" content="1200" />
@@ -24,6 +24,6 @@
 	<meta name="twitter:card" content="summary_large_image"/>
 	<meta name="twitter:title" content="Omar & Fatima Wedding"/>
 	<meta name="twitter:description" content="23 August 2026 Jupiter Bnachei, Lebanon"/>
-	<meta name="twitter:image" content="https://fatimaomar-wedding.vercel.app/envEn1.jpeg"/>
+	<meta name="twitter:image" content="https://fatimaomar-wedding.vercel.app/card.jpeg"/>
 </svelte:head>
 {@render children()}
