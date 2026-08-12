9c2996f533dd7255492ad6f49c73e2d34c22c563
vercelsetup-pat <vercelsetup@gmail.com>
2026-08-12 08:34:46 +0300

    test image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,15 @@
 </script>
 
 <svelte:head>
-	<title>Jebrine &yrine Wedding</title>
+	<title>Omar & Fatima</title>
 
-	<meta name="description" content="Join  Jebrine & Cyrine's wedding on 13 September 2026 at Green Land Akkar Edbel,Lebanon."	/>
+	<meta name="description" content="Join Omar & Fatima wedding on 23 August 2026 at Jupiter Bnachei,Lebanon."	/>
 
 	<meta property="og:type" content="website" />
-	<meta property="og:title" content="Jebrine & Cyrine Wedding" />
-	<meta property="og:description" content="13 September 2026 • Green Land, Akkar Edbel, Lebanon"/>
-	<meta property="og:image" content="https://fatimaomar-wedding.vercel.app/card.png"/>
-	<meta property="og:image:secure_url" content="https://fatimaomar-wedding.vercel.app/card.png"/>
+	<meta property="og:title" content="Omar & Fatima Wedding" />
+	<meta property="og:description" content="23 August 2026 • Jupiter Bnachei Lebanon"/>
+	<meta property="og:image" content="https://fatimaomar-wedding.vercel.app/envEn1.jpeg"/>
+	<meta property="og:image:secure_url" content="https://fatimaomar-wedding.vercel.app/envEn1.jpeg"/>
 
 	<meta property="og:image:type" content="image/png" />
 	<meta property="og:image:width" content="1200" />
@@ -22,9 +22,9 @@
 	<meta property="og:url" content="https://fatimaomar-wedding.vercel.app/"/>
 
 	<meta name="twitter:card" content="summary_large_image"/>
-	<meta name="twitter:title" content="Ali & Mireille Wedding"/>
-	<meta name="twitter:description" content="13 September 2026 Green Land, Akkar Edbel, Lebanon"/>
-	<meta name="twitter:image" content="https://fatimaomar.vercel.app/card.png"/>
+	<meta name="twitter:title" content="Omar & Fatima Wedding"/>
+	<meta name="twitter:description" content="23 August 2026 Jupiter Bnachei, Lebanon"/>
+	<meta name="twitter:image" content="https://fatimaomar.vercel.app/envEn1.jpeg"/>
 </svelte:head>
 
 {@render children()}
